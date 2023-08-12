28d1e7b727948782ad1447bd9fb2d7bbcc3f5e02
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-08-13 05:53:30 +0630

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::sync::Arc;
+extern crate dotenv;
 
 use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use std::{env, sync::Arc};
 use tokio_postgres::NoTls;
 
 mod handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let conn = "host=150.95.82.125 port=5432 user=postgres password=P@ssword dbname=meloflow_db";
-    let (client, connection) = tokio_postgres::connect(conn, NoTls).await.unwrap();
+    dotenv().ok();
+    let conn = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let (client, connection) = tokio_postgres::connect(conn.as_str(), NoTls).await.unwrap();
     let client = Arc::new(client);
 
     // The connection object performs the actual communication with the database,
